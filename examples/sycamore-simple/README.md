d64eb7358542a950b7041649583cf382b8f03f17
Valery Stepanov <etonomick@gmail.com>
2022-12-21 16:41:29 +0400

    Sycamore basic example

---
+++ examples/sycamore-simple/README.md
@@ -0,0 +1,15 @@
+## Sycamore with Mapbox Basic Example
+
+In order to run this example you need to specify an [Mapbox access token](https://account.mapbox.com/access-tokens)
+
+```
+MAPBOX_TOKEN={ACCESS_TOKEN} trunk serve
+```
+
+or using PowerShell
+
+```
+$Env:MAPBOX_TOKEN="{ACCESS_TOKEN}"; trunk serve
+```
+
+Open http://127.0.0.1:8080 in your browser
---
+++ examples/sycamore-simple/src/main.rs
@@ -0,0 +1,26 @@
+use mapboxgl::{LngLat, MapFactory, MapOptions};
+use sycamore::prelude::*;
+use wasm_bindgen::{prelude::Closure, JsCast};
+use web_sys::window;
+
+fn main() {
+    window().unwrap().set_onload(Some(
+        Closure::<dyn Fn()>::new(move || {
+            let _map = MapFactory::new(
+                MapOptions::new(std::env!("MAPBOX_TOKEN").into(), "map".into())
+                    .center(LngLat::new(-88.6867772119069, 41.989067254515696))
+                    .zoom(12.0),
+            )
+            .unwrap();
+        })
+        .into_js_value()
+        .as_ref()
+        .unchecked_ref(),
+    ));
+    sycamore::render(|cx| {
+        view! {
+            cx,
+            div(id="map", style="width: 100%; height: 100vh")
+        }
+    })
+}
