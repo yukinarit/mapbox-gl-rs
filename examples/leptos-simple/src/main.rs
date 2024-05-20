df0608c96c3c0fbf87a08eb13b1e1c892864afe2
RmStorm <roaldstorm@gmail.com>
2024-05-20 22:15:40 +0200

    Update the leptos example

--- examples/leptos-simple/src/main.rs
+++ examples/leptos-simple/src/main.rs
@@ -1,18 +1,28 @@
 use leptos::*;
 use mapboxgl::{LngLat, Map, MapOptions};
 
-fn main() {
-    let token = std::env!("MAPBOX_TOKEN");
-    mount_to_body(|cx| view! { cx,  <Map/> });
-    let _map = Map::new(
-        MapOptions::new(token.into(), "map".into())
-            .center(LngLat::new(139.7647863, 35.6812373))
-            .zoom(15.0),
-    )
-    .unwrap();
+pub fn main() {
+    leptos::mount_to_body(|| view! { <MapComponent/> });
 }
 
 #[component]
-fn Map(cx: Scope) -> impl IntoView {
-    view! {cx, <div id="map" style="width: 100%; height: 100vh"/>}
+fn MapComponent() -> impl IntoView {
+    // Make a signal that can store the map inside the reactive system..
+    // The map has to _live_ for it to be able track its handlers and stuff!
+    let map_store = create_rw_signal(None);
+    let map_ref = create_node_ref::<html::Div>();
+    map_ref.on_load(move |m| {
+        let _map_el = m.on_mount(move |map| {
+            let token = "pk.Your.Token";
+            let map = Map::new(
+                MapOptions::new(token.into(), map.get_attribute("id").unwrap())
+                    .center(LngLat::new(10.7, 59.9))
+                    .zoom(12.0),
+            )
+            .unwrap();
+            // Persist the map into the reactive system
+            map_store.set(Some(map));
+        });
+    });
+    view! {<div id="map" style="position: absolute; top: 0; bottom: 0; width: 100%;" node_ref=map_ref/>}
 }
